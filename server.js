5fc5009ec2c303fcef72c20bb193a3d6dee92b3a
yuvalbl <yuval.bar.levi@gmail.com>
2018-08-08 20:06:20 +0300

    add json support

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+require('./common.middlewares').addCommonMiddlewares(app);
+const dataService = require('./data.service');
 
-// json parser middleware
-app.use(express.json());
-// static files middleware
-app.use(express.static(path.join(__dirname, 'public')));
-
-// static file routing
-app.get('/', (req, res, next) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
-	next();
+dataService.readJson('users', (data) => {
+  console.log('Ta da!');
+  console.log(data)
 });
 
-app.get('/register', (req, res, next) => {
-	res.sendFile(path.join(__dirname, 'public/register.html'));
-	next();
+app.get('/', (req, res) => {
+  res.redirect('/home');
 });
 
-app.get('/login', (req, res, next) => {
-	res.sendFile(path.join(__dirname, 'public/login.html'));
-	next();
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/login.html'));
+});
 
 app.listen(3000,
 	() => console.log('server listening on port 3000')
