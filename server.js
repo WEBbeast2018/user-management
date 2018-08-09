fa33fa8002e1794b92030904eec7ce2a4b88faf3
yuvalbl <yuval.bar.levi@gmail.com>
2018-08-09 23:59:00 +0300

    removing comments

--- server.js
+++ server.js
@@ -6,11 +6,6 @@ require('./common.middlewares').addCommonMiddlewares(app);
 // routes
 app.use('/register', require('./routes/register.route'));
 
-// dataService.readJson('users', (data) => {
-//   console.log('Ta da!');
-//   console.log(data)
-// });
-
 app.get('/', (req, res) => {
   res.redirect('/home');
 });
@@ -18,23 +13,6 @@ app.get('/', (req, res) => {
 app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-//
-// app.route('/register')
-// 	.get((req, res) => {
-// 		res.sendFile(path.join(__dirname, 'public/register.html'));
-// 	})
-// 	.post((req, res) => {
-// 		const newUser = {
-// 			email: req.body['email'],
-// 			password: req.body['psw']
-// 		};
-//
-// 		dataService.readJson('users', (users) => {
-// 			users.push(newUser);
-// 			dataService.writeJson('users', users, () => res.redirect('/home'));
-// 		});
-// 	});
-//
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/login.html'));
